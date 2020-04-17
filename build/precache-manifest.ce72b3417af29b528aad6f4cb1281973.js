221ccd4212528dbcd951cda1b76087015b0e3e9f
ELBEQQAL94 <ELBEQQAL.youssef@gmail.com>
2020-04-17 13:29:19 +0100

    first commit

---
+++ build/precache-manifest.ce72b3417af29b528aad6f4cb1281973.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20539a80534d089de63e8e01cc34c68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0186f2bd59d12ebc75ee",
+    "url": "/static/css/main.38fa2a99.chunk.css"
+  },
+  {
+    "revision": "f1100f2cd8f93f0d399a",
+    "url": "/static/js/2.f8b03dd8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.f8b03dd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0186f2bd59d12ebc75ee",
+    "url": "/static/js/main.ff42f1a5.chunk.js"
+  },
+  {
+    "revision": "46200cf817cd5dfad81a",
+    "url": "/static/js/runtime-main.ef837b83.js"
+  }
+]);
